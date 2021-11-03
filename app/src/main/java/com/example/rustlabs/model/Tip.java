701683211236f8e5a9e52e7176cb0babeaa44df1
ljkeller <kellerjlucas@gmail.com>
2021-11-03 18:05:06 -0500

    Added firestore dependencies & updated tip class

--- app/src/main/java/com/example/rustlabs/model/Tip.java
+++ app/src/main/java/com/example/rustlabs/model/Tip.java
@@ -3,6 +3,7 @@ package com.example.rustlabs.model;
 import android.text.TextUtils;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.ServerTimestamp;
 
 import java.util.Date;
 
@@ -14,21 +15,19 @@ public class Tip
 
     private String userId;
     private String userName;
-    private double rating;
     private String text;
     private @ServerTimestamp
     Date timestamp;
 
-    public Rating() {}
+    public Tip() {}
 
-    public Rating(FirebaseUser user, double rating, String text) {
+    public Tip(FirebaseUser user, String text) {
         this.userId = user.getUid();
         this.userName = user.getDisplayName();
         if (TextUtils.isEmpty(this.userName)) {
             this.userName = user.getEmail();
         }
 
-        this.rating = rating;
         this.text = text;
     }
 
@@ -46,14 +45,6 @@ public class Tip
 
     public void setUserName(String userName) {
         this.userName = userName;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
     }
 
     public String getText() {
