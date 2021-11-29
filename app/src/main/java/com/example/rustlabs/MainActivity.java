83bfb68a292b3390e78cebcf4c7a483a5e9b7573
ljkeller <kellerjlucas@gmail.com>
2021-11-29 13:43:23 -0600

    Added new navigation icons, names, destinations. Working on using AuthUI but experiencing bugs.

--- app/src/main/java/com/example/rustlabs/MainActivity.java
+++ app/src/main/java/com/example/rustlabs/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.rustlabs;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.rustlabs.viewmodel.MainActivityViewModel;
@@ -15,10 +15,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.rustlabs.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -29,6 +26,25 @@ public class MainActivity extends AppCompatActivity
     private FirebaseFirestore mFirestore;
 
     private MainActivityViewModel mViewModel;
+
+    private int RC_SIGN_IN = 1822;
+
+//    private void onSignInResult(FirebaseAuthUIAuthenticationResult result)
+//    {
+//        IdpResponse response = result.getIdpResponse();
+//        if (result.getResultCode() == RESULT_OK) {
+//            // Successfully signed in
+//            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//            Toast.makeText(this, "Sign in successful", Toast.LENGTH_SHORT);
+//            // ...
+//        } else {
+//            Toast.makeText(this, "Sign in un-successful", Toast.LENGTH_SHORT);
+//            // Sign in failed. If response is null the user canceled the
+//            // sign-in flow using the back button. Otherwise check
+//            // response.getError().getErrorCode() and handle the error.
+//            // ...
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -47,7 +63,7 @@ public class MainActivity extends AppCompatActivity
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_search, R.id.navigation_weapon, R.id.navigation_structures)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
@@ -59,12 +75,24 @@ public class MainActivity extends AppCompatActivity
     {
         super.onStart();
 
-        // Start sign in if necessary
-        if (shouldStartSignIn())
-        {
-            startSignIn();
-            return;
-        }
+//        // Choose authentication providers
+//        List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                new AuthUI.IdpConfig.EmailBuilder().build());
+//
+//        // Create and launch sign-in intent
+//        Intent signInIntent = AuthUI.getInstance()
+//                .createSignInIntentBuilder()
+//                .setAvailableProviders(providers)
+//                .build();
+//        signInLauncher.launch(signInIntent);
+//
+//
+//        // Start sign in if necessary
+//        if (shouldStartSignIn())
+//        {
+//            startSignIn();
+//            return;
+//        }
 
 //        // Apply filters
 //        onFilter(mViewModel.getFilters());
@@ -86,12 +114,22 @@ public class MainActivity extends AppCompatActivity
 //        }
     }
 
+//    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+//            new FirebaseAuthUIActivityResultContract(),
+//            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+//                @Override
+//                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+//                    onSignInResult(result);
+//                }
+//            }
+//    );
+
     private boolean shouldStartSignIn()
     {
         return (!mViewModel.getIsSigningIn() && mAuth.getCurrentUser() == null);
     }
 
-    private void startSignIn()
+    public void startSignIn(View view)
     {
 //        // Sign in with FirebaseUI
 //        Intent intent = FirebaseUtil.getAuthUI()
--- app/src/main/java/com/example/rustlabs/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/rustlabs/ui/home/HomeFragment.java
@@ -1,11 +1,19 @@
 package com.example.rustlabs.ui.home;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,6 +22,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.rustlabs.R;
 import com.example.rustlabs.databinding.FragmentHomeBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeFragment extends Fragment
 {
@@ -39,6 +56,25 @@ public class HomeFragment extends Fragment
                 textView.setText(s);
             }
         });
+
+        Button signInButton = (Button) binding.authButton;
+        signInButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Choose authentication providers
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build());
+
+                // Create and launch sign-in intent
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build().setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+                signInLauncher.launch(signInIntent);
+            }
+        });
         return root;
     }
 
@@ -47,5 +83,33 @@ public class HomeFragment extends Fragment
     {
         super.onDestroyView();
         binding = null;
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result)
+    {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Toast.makeText(super.getContext(), "Sign in successful", Toast.LENGTH_SHORT);
+//            Toast.makeText(this, "Sign in successful", Toast.LENGTH_SHORT);
+            // ...
+        } else {
+            Toast.makeText(super.getContext(), "Sign in un-successful", Toast.LENGTH_SHORT);
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 }
