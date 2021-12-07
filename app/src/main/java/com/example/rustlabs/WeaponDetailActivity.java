ee9f162e931aa9563d1e07260e96d1804fa5e722
ljkeller <kellerjlucas@gmail.com>
2021-12-07 00:23:55 -0600

    WIP implementation of weapon detail activity

--- app/src/main/java/com/example/rustlabs/WeaponDetailActivity.java
+++ app/src/main/java/com/example/rustlabs/WeaponDetailActivity.java
@@ -1,25 +1,65 @@
 package com.example.rustlabs;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.hardware.input.InputManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.rustlabs.adapter.TipAdapter;
 import com.example.rustlabs.model.Tip;
 import com.example.rustlabs.model.Weapon;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+
+import org.w3c.dom.Document;
 
 public class WeaponDetailActivity extends AppCompatActivity implements View.OnClickListener,
         EventListener<DocumentSnapshot>, TipDialogFragment.TipListener
 {
+
+    private static final String TAG = "WeaponDetailActivity";
+
+    public static final String KEY_WEAPON_ID = "key_weapon_id";
+
+    // UI Members
+    private ImageView mImageView;
+    private TextView mNameView;
+    private TextView mNumTipsView;
+    private TextView mAmmoTypeView;
+    private TextView mDamageView;
+    private TextView mTopLocationView;
+    private ViewGroup mEmptyView;
+    private RecyclerView mTipRecycler;
+
+    // Fragment Members
+    private TipDialogFragment mTipDialog;
+
+    // Firestore Members
+    private FirebaseFirestore mFirestore;
+    private DocumentReference mWeaponRef;
+    private ListenerRegistration mWeaponRegistration;
+
+    // Internal members
+    private TipAdapter mTipAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,18 +72,38 @@ public class WeaponDetailActivity extends AppCompatActivity implements View.OnCl
     protected void onStart()
     {
         super.onStart();
+
+        mTipAdapter.startListening();
+        mWeaponRegistration = mWeaponRef.addSnapshotListener(this)
     }
 
     @Override
     protected void onStop()
     {
         super.onStop();
+
+        mTipAdapter.stopListening();
+
+        if (mWeaponRegistration != null)
+        {
+            mWeaponRegistration.remove();
+            mWeaponRegistration = null;
+        }
     }
 
     @Override
     public void onClick(View v)
     {
         // TODO: SWITCH
+        switch (v.getId())
+        {
+            case R.id.weapon_button_back:
+                onBackArrowClicked(v);
+                break;
+            case R.id.show_tip_dialog:
+                onAddTipClicked(v);
+                break;
+        }
     }
 
     private Task<Void> addTip(final DocumentReference tipRef, final Tip tip)
@@ -53,14 +113,28 @@ public class WeaponDetailActivity extends AppCompatActivity implements View.OnCl
     }
 
     @Override
-    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error)
+    public void onEvent(@Nullable DocumentSnapshot snapshot,
+                        @Nullable FirebaseFirestoreException error)
     {
         //TODO: Implement
+        if (error != null)
+        {
+            Log.w(TAG, "onEvent: error", error);
+            return;
+        }
+
+        onWeaponLoaded(snapshot.toObject(Weapon.class));
     }
 
     private void onWeaponLoaded(Weapon weapon)
     {
-        //TODO: Implement
+        mNameView.setText(weapon.getName());
+        mNumTipsView.setText(String.valueOf(weapon.getNumTips()));
+        mAmmoTypeView.setText(weapon.getAmmoType());
+        mDamageView.setText(String.valueOf(weapon.getDamage()));
+        mTopLocationView.setText(weapon.getTopLocation());
+
+        Glide.with(mImageView.getContext()).load(weapon.getPicture()).into(mImageView);
     }
 
     public void onBackArrowClicked(View view)
@@ -70,13 +144,37 @@ public class WeaponDetailActivity extends AppCompatActivity implements View.OnCl
 
     public void onAddTipClicked(View view)
     {
-        //TODO: Implement
+        mTipDialog.show(getSupportFragmentManager(), TipDialogFragment.TAG);
     }
 
     @Override
     public void onTip(Tip tip)
     {
-        //TODO: Implement
+        // Must add rating and update count
+        addTip(mWeaponRef, tip).addOnSuccessListener(this, new OnSuccessListener<Void>()
+        {
+            @Override
+            public void onSuccess(Void unused)
+            {
+                Log.d(TAG, "onSuccess: Tip Added");
+
+                // Hide keyboard and scroll top
+                hideKeyboard();
+                mTipRecycler.smoothScrollToPosition(0);
+            }
+        }).addOnFailureListener(this, new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                Log.w(TAG, "onFailure: Add Tip failed", e);
+
+                // Show failure message & scroll top
+                hideKeyboard();
+                Snackbar.make(findViewById(android.R.id.content), "Failed to add raiting",
+                              Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void hideKeyboard()
